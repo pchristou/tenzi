daac52c3a04310fc77fbfbaa114dec71f9f83434
pchristou <pierischristou@gmail.com>
2025-06-11 20:26:18 +0100

    chore: cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ScratchPad from './0-playground/App.jsx';
-import StaticApp from './1-static/App.jsx';
-import DataDrivenApp from './2-data-driven/App.jsx';
-import ChefApp from './3-state/App.jsx';
-import SoundPad from './0-playground/components/SoundPad.jsx';
-import StarWars from './0-playground/components/StarWars.jsx';
-import WindowTracker from './0-playground/components/WindowTrackerApp.jsx';
-import MemeGeneratorApp from './4-side-effects/App.jsx';
+import {JSX } from 'react';
 import Tenzies from './5-tenzies/App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <>
-        {/*<StaticApp />*/}
-        {/*<DataDrivenApp />*/}
-        {/*<ChefApp />*/}
-        {/*{<SoundPad darkMode={true} />}*/}
-        {/*<MemeGeneratorApp />*/}
-        {/*<WindowTracker />*/}
-        {/*<StarWars />*/}
         {<Tenzies />}
     </>
 )
